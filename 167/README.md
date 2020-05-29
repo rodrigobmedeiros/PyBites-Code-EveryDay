14a911a5b87decc3b5b22b988eb273ae4d016c1d
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2020-05-29 19:35:02 -0300

    solve bite 167

---
+++ 167/README.md
@@ -0,0 +1,9 @@
+## [Bite 167. Complete a User class: properties and representation dunder methods](https://codechalleng.es/bites/167/)
+
+Good luck and please share you code in the Bite forums upon completion.
+
+For questions use [our Slack](https://pybites.slack.com/archives/C6BGDQQ3B) (no spoilers please).
+
+Check out our full catalogue of Bites of Py [here](https://codechalleng.es/bites/catalogue).
+
+Enjoy and keep calm and code in Python!
---
+++ 167/user.py
@@ -0,0 +1,45 @@
+class User:
+    """A User class
+       (Django's User model inspired us)
+    """
+
+    def __init__(self, first_name, last_name):
+        """Constructor, base values"""
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @property
+    def get_full_name(self):
+        """Return first separated by a whitespace
+           and using title case for both.
+        """
+        complete_name = self.first_name + ' ' + self.last_name
+        return complete_name.title()
+
+    @property
+    def username(self):
+        """A username consists of the first char of
+           the user's first_name and the first 7 chars
+           of the user's last_name, both lowercased.
+
+           If this is your first property, check out:
+           https://pybit.es/property-decorator.html
+        """
+        user_name = self.first_name[:3].lower() + self.last_name[:7].lower()
+        return user_name
+
+    # add a __str__ and a __repr__
+    # see: https://stackoverflow.com/a/1438297
+    # "__repr__ is for devs, __str__ is for customers"
+    #
+    # see also TESTS for required output
+
+    def __str__(self):
+        return f'{self.get_full_name()} ({self.username()})'
+
+    def __repr__(self):
+        """Don't hardcode the class name, hint: use a
+           special attribute of self.__class__ ...
+        """
+        return f'User({self.first_name.lower()}, {self.last_name.lower()})'
+
