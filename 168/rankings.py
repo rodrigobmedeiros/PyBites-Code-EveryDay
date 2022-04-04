a7a78cdc21feb4560ee60e6bb378338696da8119
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-04-03 21:50:27 -0300

    start bite 168

---
+++ 168/rankings.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+from typing import List, Tuple
+
+bites: List[int] = [283, 282, 281, 263, 255, 230, 216, 204, 197, 196, 195]
+names: List[str] = [
+    "snow",
+    "natalia",
+    "alex",
+    "maquina",
+    "maria",
+    "tim",
+    "kenneth",
+    "fred",
+    "james",
+    "sara",
+    "sam",
+]
+
+
+@dataclass
+class Ninja:
+    """
+    The Ninja class will have the following features:
+
+    string: name
+    integer: bites
+    support <, >, and ==, based on bites
+    print out in the following format: [469] bob
+    """
+    pass
+
+
+@dataclass
+class Rankings:
+    """
+    The Rankings class will have the following features:
+
+    method: add() that adds a Ninja object to the rankings
+    method: dump() that removes/dumps the lowest ranking Ninja from Rankings
+    method: highest() returns the highest ranking Ninja, but it takes an optional
+            count parameter indicating how many of the highest ranking Ninjas to return
+    method: lowest(), the same as highest but returns the lowest ranking Ninjas, also
+            supports an optional count parameter
+    returns how many Ninjas are in Rankings when len() is called on it
+    method: pair_up(), pairs up study partners, takes an optional count
+            parameter indicating how many Ninjas to pair up
+    returns List containing tuples of the paired up Ninja objects
+    """
+    pass
