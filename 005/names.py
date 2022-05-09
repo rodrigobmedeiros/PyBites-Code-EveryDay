7cffb43a768cb00cf944fdd72049566baca7b175
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-05-09 15:28:18 -0300

    fixing problem with find shortest first name function

--- 005/names.py
+++ 005/names.py
@@ -10,25 +10,21 @@ def dedup_and_title_case_names(names: List[str]) -> List[str]:
     """Should return a list of title cased names,
        each name appears only once"""
 
-    # Remove Duplications
-    names = list(set(names))
-    names = [name.title() for name in names]
+    names = [name.title() for name in set(names)]
     return names
 
 def sort_by_surname_desc(names: List[str]) -> List[str]:
     """Returns names list sorted desc by surname"""
     names = dedup_and_title_case_names(names)
-    sorted_names = sorted(names, key=lambda name: name.split(' ')[-1], reverse=True)
-    return sorted_names
+    
+    return sorted(
+        names, key=lambda name: name.split()[-1], reverse=True
+    )
 
 def shortest_first_name(names: List[str]) -> List[str]:
     """Returns the shortest first name (str).
        You can assume there is only one shortest name.
     """
     names = dedup_and_title_case_names(names)
-    shortest_first_name = min(names, key=lambda name: len(name)).split(' ')[0]
-
-    return shortest_first_name
-
-nome = 'rodrigo bernardo'
-print(nome.title())
+    names = [name.split()[0] for name in names]
+    return min(names, key=len)
