2c33eb9df6336db2795ec89e947a3b9757d69442
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2022-05-12 11:58:08 -0300

    implement solution for bite 015

--- 015/enumerate_data.py
+++ 015/enumerate_data.py
@@ -1,5 +1,7 @@
-names = 'Julian Bob PyBites Dante Martin Rodolfo'.split()
-countries = 'Australia Spain Global Argentina USA Mexico'.split()
+from typing import List
+
+names: List[str] = 'Julian Bob PyBites Dante Martin Rodolfo'.split()
+countries: List[str] = 'Australia Spain Global Argentina USA Mexico'.split()
 
 
 def enumerate_names_countries():
@@ -10,4 +12,9 @@ def enumerate_names_countries():
        4. Dante      Argentina
        5. Martin     USA
        6. Rodolfo    Mexico"""
-    pass
+    
+    for idx in range(len(names)):
+       print(f'{idx + 1}. {names[idx].ljust(11)}{countries[idx]}')
+
+
+enumerate_names_countries()
