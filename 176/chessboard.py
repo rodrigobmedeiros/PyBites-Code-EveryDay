7193749d342e4b1583908e60a6391c888d551802
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-05-17 23:03:38 -0300

    Add comments about pybites solution

--- 176/chessboard.py
+++ 176/chessboard.py
@@ -19,3 +19,26 @@ def create_chessboard(size=8):
             is_reverse = False
     
 create_chessboard(size=8)
+
+
+# Solution
+# The solution creates two loops, mine creates only one
+# I decided to create a line and reverse this line when needed.
+# I really have to think better about O(n)
+# But I think is a good solution.
+
+
+# def create_chessboard(size=8):
+#     """Create a chessboard with of the size passed in.
+#     Don't return anything, print the output to stdout"""
+#     board = ''
+
+#     for i in range(size):
+#         for j in range(size):
+#             if (i + j) % 2 == 0:
+#                 board += ' '
+#             else:
+#                 board += '#'
+#         board += '\n'
+
+#     print(board)
