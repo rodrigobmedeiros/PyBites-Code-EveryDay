b9f920b9b1d8254528dd7cec75181500ad97dd32
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-05-26 16:26:29 -0300

    Solve bite 210

---
+++ 210/annotations.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def sum_numbers(numbers: List[int]) -> int:
+    """Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+    :raises TypeError: if not all numeric values passed in
+    :return: sum of numbers
+    :rtype: int
+    """
+    pass
+
